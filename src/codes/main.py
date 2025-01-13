5084d3488074938113d73481c0f9a6fe44de8d78
TomiJolkkonen <tomi.jolkkonen@skillio.ai>
2025-01-13 14:37:15 +0200

    pieniä muutoksia pull-requestiin

--- src/codes/main.py
+++ src/codes/main.py
@@ -1,5 +1,5 @@
 def pullRequestDemo():
-    print("This is a pull request demo")
+    print("This is a pull request demo and this is a change to it")
     
 if __name__ == "__main__":
     pullRequestDemo()
